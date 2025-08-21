6dc62067b40a77f413149c56508eae211f3108af
Ubuntu <ubuntu@ip-172-31-27-116.us-east-2.compute.internal>
2025-08-21 09:31:42 +0000

    Mod 9 initial upload

---
+++ mod9/forkEtherAAVEFlash/hardhat.config.js
@@ -0,0 +1,21 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+        networks: {
+                hardhat: {
+                localhost: {
+                        url: "https://ec2-52-10-54-72.us-west-2.compute.amazonaws.com:8545"
+,                       chainId: 31337
+        
+                }
+,
+                        forking: {
+                        url: "https://cloudflare-eth.com/"
+,                       chainId: 31337
+                    }
+
+                }
+   },
+	solidity: "0.8.10",
+};
